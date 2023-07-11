b09717c04756d22d27ce493b63c39f109a495dea
Rasmus Jensen <rasmusdj96@gmail.com>
2023-07-11 22:17:52 +0300

    Cleaned up login logic

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
@@ -6,6 +7,7 @@ const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export const Login = () => {
+	const navigate = useNavigate();
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [error, setError] = useState(null);
@@ -22,50 +24,12 @@ export const Login = () => {
 			if (error) {
 				setError(error.message);
 			} else {
-				const { data: userData, error: userError } =
-					await supabase.auth.getUser();
-
-				if (userError) {
-					setError(userError.message);
-				} else {
-					const { user } = userData;
-					console.log(user.user_metadata);
-
-					// Save user_metadata to localStorage with expiration
-					const expirationTimestamp = Math.floor(Date.now() / 1000) + 3600; // 3600 seconds = 1 hour
-					const metadataWithExpiration = {
-						user_metadata: user.user_metadata,
-						expirationTimestamp: expirationTimestamp,
-					};
-					localStorage.setItem(
-						"user_metadata",
-						JSON.stringify(metadataWithExpiration)
-					);
-				}
+				navigate("/home");
 			}
 		} catch (error) {
 			setError(error.message);
 		}
 	};
-
-	useEffect(() => {
-		const checkLocalStorageExpiration = () => {
-			const storedMetadata = localStorage.getItem("user_metadata");
-			if (storedMetadata) {
-				const { user_metadata, expirationTimestamp } =
-					JSON.parse(storedMetadata);
-				const currentTime = Math.floor(Date.now() / 1000);
-				if (currentTime > expirationTimestamp) {
-					// Clear localStorage if expiration time has passed
-					localStorage.removeItem("user_metadata");
-				} else {
-					console.log(user_metadata);
-				}
-			}
-		};
-
-		checkLocalStorageExpiration();
-	}, []);
 
 	return (
 		<div>
